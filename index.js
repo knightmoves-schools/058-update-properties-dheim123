5be392b103d80744130c637da03b8a77b1201318
dheim123 <dheim@midland.k12.ia.us>
2025-11-13 12:09:45 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+var person = {
+  'age': 0,
+};
 
+function celebrateBirthday (person) {
+  person.age = 1;
+  return person
+}
+
+
+
+
+//should create a person object that has an age of 0 (zero) 
+//should create a function named celebrateBirthday that takes a person, updates their age by one year and returns the person
